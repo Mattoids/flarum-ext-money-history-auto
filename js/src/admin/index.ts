aeb3e394eac2ba61a36790a3b1e3a5668be37459
liufei <liufei@liufei.info>
2023-12-12 14:03:10 +0800

    提交

---
+++ js/src/admin/index.ts
@@ -0,0 +1,6 @@
+import app from 'flarum/admin/app';
+
+app.initializers.add('mattoid/flarum-ext-money-history-auto', () => {
+  console.log('[mattoid/flarum-ext-money-history-auto] Hello, admin!');
+});
+
---
+++ js/src/forum/index.ts
@@ -0,0 +1,5 @@
+import app from 'flarum/forum/app';
+
+app.initializers.add('mattoid/flarum-ext-money-history-auto', () => {
+  console.log('[mattoid/flarum-ext-money-history-auto] Hello, forum!');
+});
